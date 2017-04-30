d9fac06a144dea1b7e3b46cf8999e8b0a99bfa71
Yahya Gilany <yahya.gilany@live.com>
2017-04-30 14:46:30 -0400

    refactored MainPage > OldMainPage

--- Calculator/Calculator/App.cs
+++ Calculator/Calculator/App.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,7 @@ namespace Calculator
 		public App()
 		{
 			// The root page of your application
-            MainPage = new MainPage();
+            MainPage = new OldMainPage();
 		}
 
 		protected override void OnStart()
